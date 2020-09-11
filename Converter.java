f3e50ad981f8b7f1cd8a7edb16affa5f977e4965
Ryan Raimondo <ryan.raimondo@ucalgary.ca>
2020-09-11 11:19:00 -0600

    Create Converter.java

---
+++ Converter.java
@@ -0,0 +1,21 @@
+public class Converter {
+    //Your names go here:
+    /*
+     * @Author: Ryan Raimondo
+     * Name of the  student
+     * Name of the first student
+     *
+     */
+    private double celsiusToFahrenheit(double C){
+        //TODO: The third student will implement this method
+        return 0;
+    }
+    private double fahrenheitToCelsius(double F){
+        //TODO: The second student will implement this method
+
+    }
+    public static void main(String[] args) {
+        celsiusToFahrenheit(180);
+        fahrenheitToCelsius(250);
+    }
+}
